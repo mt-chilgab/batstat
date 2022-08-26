c38a7cd81746a25a2741f3a0633abbfd542f8d12
mt-chilgab <tireburner1999@gmail.com>
2022-08-26 19:53:36 +0900

    test

---
+++ adc_master_side/CMakeFiles/QVoltageIndicator.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubuntu/batstat/adc_master_side/QVoltageIndicator_autogen/mocs_compilation.cpp" "/home/ubuntu/batstat/adc_master_side/CMakeFiles/QVoltageIndicator.dir/QVoltageIndicator_autogen/mocs_compilation.cpp.o"
+  "/home/ubuntu/batstat/adc_master_side/src/AsyncSerial/AsyncSerial.cpp" "/home/ubuntu/batstat/adc_master_side/CMakeFiles/QVoltageIndicator.dir/src/AsyncSerial/AsyncSerial.cpp.o"
+  "/home/ubuntu/batstat/adc_master_side/src/QtImpl/QAsyncSerial.cpp" "/home/ubuntu/batstat/adc_master_side/CMakeFiles/QVoltageIndicator.dir/src/QtImpl/QAsyncSerial.cpp.o"
+  "/home/ubuntu/batstat/adc_master_side/src/QtImpl/QVoltageIndicatorApp.cpp" "/home/ubuntu/batstat/adc_master_side/CMakeFiles/QVoltageIndicator.dir/src/QtImpl/QVoltageIndicatorApp.cpp.o"
+  "/home/ubuntu/batstat/adc_master_side/src/Socket/Client.cpp" "/home/ubuntu/batstat/adc_master_side/CMakeFiles/QVoltageIndicator.dir/src/Socket/Client.cpp.o"
+  "/home/ubuntu/batstat/adc_master_side/src/main.cpp" "/home/ubuntu/batstat/adc_master_side/CMakeFiles/QVoltageIndicator.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ALL_NO_LIB"
+  "BOOST_DATE_TIME_DYN_LINK"
+  "BOOST_SYSTEM_DYN_LINK"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "QVoltageIndicator_autogen/include"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
